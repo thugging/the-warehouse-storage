71b4848d43147661d7d6fae7bd3937db24ec936e
thugging <93628674+thugging@users.noreply.github.com>
2026-01-25 17:05:56 -0600

    group

---
+++ archive/dumps/megahubdump.lua
@@ -0,0 +1,10 @@
+--//https://scriptblox.com/script/Universal-Script-Mega-Hub-l-BEST-SCRIPTS-l-AUTO-FARMS-l-AND-WAY-MORE-92437
+--//Starts WeAreDevs and then swityches to an unkown obfuscator
+--//Creator: tacarra0
+
+URLs Found:
+https://sirius.menu/rayfield
+
+Dumped:
+https://pastefy.app/rOCSgZde/raw --//His Key-system
+https://pastefy.app/Jc57GCv2/raw --//His Script Hub
