8b4d1d5477ca6addc0b2459ff49bf5e055d6d17b
sigmasigmaseperateacc11141 <luasploitt@gmail.com>
2026-01-25 23:48:12 +0000

    group

---
+++ archive/dumps/InfiniteMoney.lua
@@ -0,0 +1,11 @@
+--//https://scriptblox.com/script/Build-a-Stone-Miner!-Infinite-money-75223
+--//Obfuscated with WeAreDevsObfuscator: 
+"https://wearedevs.net/obfuscator"
+--//Creator: Coderk
+
+--//URLs found in the dump:
+"https://sirius.menu/rayfield"
+"https://games.roblox.com/v1/games/125563061751450/servers/Public?sortOrder=Asc&limit=100"
+
+Dumped:
+"https://pastefy.app/sz44r6Ax/raw"
