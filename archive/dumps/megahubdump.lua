b056b27a31d134e54db80aaf63ade7cfd71bc93b
sigmasigmaseperateacc11141 <luasploitt@gmail.com>
2026-01-25 23:49:19 +0000

    group

--- archive/dumps/megahubdump.lua
+++ archive/dumps/megahubdump.lua
@@ -1,10 +1,11 @@
 --//https://scriptblox.com/script/Universal-Script-Mega-Hub-l-BEST-SCRIPTS-l-AUTO-FARMS-l-AND-WAY-MORE-92437
 --//Starts WeAreDevs and then swityches to an unkown obfuscator
+"https://wearedevs.net/obfuscator"
 --//Creator: tacarra0
 
 URLs Found:
-https://sirius.menu/rayfield
+"https://sirius.menu/rayfield"
 
 Dumped:
-https://pastefy.app/rOCSgZde/raw --//His Key-system
-https://pastefy.app/Jc57GCv2/raw --//His Script Hub
+"https://pastefy.app/rOCSgZde/raw" --//His Key-system
+"https://pastefy.app/Jc57GCv2/raw" --//His Script Hub
